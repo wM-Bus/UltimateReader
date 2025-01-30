98535d0242391b6beaee9308f2505e45b6ef919e
SzczepanLeon <75840303+SzczepanLeon@users.noreply.github.com>
2025-01-30 18:23:38 +0100

    Update rf_sx1276.h

--- components/wmbus/rf_sx1276.h
+++ components/wmbus/rf_sx1276.h
@@ -113,7 +113,8 @@ namespace wmbus {
       // flag to indicate that a packet was received
       bool receivedFlag = false;
 
-      SPIClass vspi{HSPI};
+      //SPIClass vspi{HSPI}; // przydziela IP ale restartuje
+      SPIClass vspi{FSPI};
       SX1276 radio = new Module(RADIO_CS_PIN, RADIO_IRQ_PIN, RADIO_RST_PIN, RADIO_BUSY_PIN, vspi);
 
       uint32_t sync_time_{0};
