9c952a989f6a37642f6afcf0f5028a6402ff000e
SzczepanLeon <75840303+SzczepanLeon@users.noreply.github.com>
2025-02-21 19:03:01 +0100

    Update __init__.py

--- components/wmbus/__init__.py
+++ components/wmbus/__init__.py
@@ -132,8 +132,7 @@ async def to_code(config):
     var = cg.new_Pvariable(config[CONF_ID])
     await cg.register_component(var, config)
 
-    print(BOARD[config[CONF_BOARD]])
-    #cg.add_define("RADIO_TYPE", cg.RawExpression(config[CONF_BOARD][RADIO_TYPE]))
+    #cg.add_define("RADIO_TYPE", cg.RawExpression(BOARD[config[CONF_BOARD]][RADIO_TYPE]))
 
     cg.add(var.add_cc1101(0, 0, 0, 0, 0, 0, config[CONF_FREQUENCY], config[CONF_SYNC_MODE]))
 
