48e5f5c5291908d8977d9937535196fd51a16dba
SzczepanLeon <75840303+SzczepanLeon@users.noreply.github.com>
2025-02-21 18:51:24 +0100

    Update __init__.py

--- components/wmbus/__init__.py
+++ components/wmbus/__init__.py
@@ -130,7 +130,7 @@ async def to_code(config):
     var = cg.new_Pvariable(config[CONF_ID])
     await cg.register_component(var, config)
 
-    cg.add_define("RADIO_TYPE", "SX1276")
+    cg.add_define("RADIO_TYPE", cg.RawExpression("SX1276"))
 
     mosi = await cg.gpio_pin_expression(config[CONF_MOSI_PIN])
     miso = await cg.gpio_pin_expression(config[CONF_MISO_PIN])
