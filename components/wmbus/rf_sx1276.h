6ee6aaf900a23c5c6927614ea3e63f858b32032d
SzczepanLeon <75840303+SzczepanLeon@users.noreply.github.com>
2025-02-21 18:07:33 +0100

    Update rf_sx1276.h

--- components/wmbus/rf_sx1276.h
+++ components/wmbus/rf_sx1276.h
@@ -100,7 +100,7 @@ namespace wmbus {
       // flag to indicate that a packet was received
       bool receivedFlag = false;
 
-      SX1276 radio = new Module(RADIO_CS_PIN, RADIO_DIO0_PIN, RADIO_RST_PIN, RADIO_DIO1_PIN);
+      SX1276 *radio{nullptr};
 
       uint32_t sync_time_{0};
       uint8_t  extra_time_{20};
