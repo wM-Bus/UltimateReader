b2d36c8b6903186487d5c231f5244e5248abc6b5
SzczepanLeon <75840303+SzczepanLeon@users.noreply.github.com>
2025-02-22 22:26:18 +0100

    Update __init__.py

--- components/wmbus/__init__.py
+++ components/wmbus/__init__.py
@@ -2,6 +2,7 @@ import esphome.codegen as cg
 import esphome.config_validation as cv
 from esphome import pins
 from esphome.log import Fore, color
+from esphome.components import spi
 from esphome.components import time
 from esphome.components import mqtt
 from esphome.components import wifi
