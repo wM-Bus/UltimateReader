9668b3a4ee08a50ff6597590dcfb848345e0081e
SzczepanLeon <75840303+SzczepanLeon@users.noreply.github.com>
2025-02-22 22:55:03 +0100

    Update __init__.py

--- components/wmbus/__init__.py
+++ components/wmbus/__init__.py
@@ -96,7 +96,7 @@ CONFIG_SCHEMA = cv.Schema({
     cv.GenerateID():                                   cv.declare_id(WMBusComponent),
     cv.OnlyWith(CONF_MQTT_ID, "mqtt"):                 cv.use_id(mqtt.MQTTClientComponent),
     cv.OnlyWith(CONF_TIME_ID, "time"):                 cv.use_id(time.RealTimeClock),
-    cv.OnlyWith(CONF_SPI_ID, "spi_bus"):                   cv.use_id(spi.SPIInterface),
+    cv.OnlyWith(CONF_SPI_ID, "spi_device"):            cv.use_id(spi.SPIInterface),
     cv.OnlyWith(CONF_WIFI_REF, "wifi"):                cv.use_id(wifi.WiFiComponent),
     cv.OnlyWith(CONF_ETH_REF, "ethernet"):             cv.use_id(ethernet.EthernetComponent),
     cv.Optional(CONF_MOSI_PIN,       default=35):      pins.internal_gpio_output_pin_schema,
