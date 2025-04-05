fd20ea7a68e9170f8c4171f88417ffa7730987c2
SzczepanLeon <75840303+SzczepanLeon@users.noreply.github.com>
2025-04-06 00:28:39 +0200

    Update rf_sx1276.h

--- components/wmbus/rf_sx1276.h
+++ components/wmbus/rf_sx1276.h
@@ -17,36 +17,31 @@
 
 #define UNUSED_PIN                   (0)
 
-#define I2C_SDA                     18
-#define I2C_SCL                     17
 #define OLED_RST                    UNUSED_PIN
 
-#define RADIO_SCLK_PIN              5
-#define RADIO_MISO_PIN              3
-#define RADIO_MOSI_PIN              6
-#define RADIO_CS_PIN                7
+#define RADIO_SCLK_PIN              10
+#define RADIO_MISO_PIN              9
+#define RADIO_MOSI_PIN              11
+#define RADIO_CS_PIN                40
+#define RADIO_RST_PIN               46
+#define RADIO_DIO0_PIN              8
+#define RADIO_DIO1_PIN              16
+
+#define LED_PIN                     38
+#define I2C_SDA                     17
+#define I2C_SCL                     18
 
 #define SDCARD_MOSI                 11
 #define SDCARD_MISO                 2
 #define SDCARD_SCLK                 14
 #define SDCARD_CS                   13
 
-#define BOARD_LED                   37
+#define BOARD_LED                   38
 #define LED_ON                      HIGH
 
 #define BUTTON_PIN                  0
 #define ADC_PIN                     1
 
-#define RADIO_RST_PIN               8
-
-#define RADIO_BUSY_PIN              33      //DIO1
-
-#define RADIO_DIO0_PIN              9
-#define RADIO_DIO1_PIN              33
-#define RADIO_DIO2_PIN              34
-#define RADIO_DIO3_PIN              21
-#define RADIO_DIO4_PIN              10
-#define RADIO_DIO5_PIN              36
 
 #define BOARD_VARIANT_NAME          "T3 S3 V1.2"
 
